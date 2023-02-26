a0e802f73eb5a920917021f60cfed3853cbe210b
Rene Santiago <rsj510@gmail.com>
2023-02-26 16:22:04 -0500

    add form to update and delete

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, Form } from "react-router-dom";
 
 const Show = () => {
     const post = useLoaderData()
@@ -12,6 +12,25 @@ const Show = () => {
         <div style={div}>
             <h1>{post.name}</h1>
             <h2>{post.description}</h2>
+            <h2>Create a Restaurant</h2>
+            <Form action={`/update/${post.id}`} method="post">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Type Name Here"
+                    defaultValue={post.name}
+                />
+                <input
+                    tyoe="text"
+                    name="description"
+                    placeholder="Type Descripttion Here"
+                    defaultValue={post.description}
+                />
+                <button>Update Restaurant</button>
+            </Form>
+            <Form action={`/delete/${post.id}`} method="post">
+                <button>Delete Restaurant</button>
+            </Form>
             <Link to="/">
                 <button>Go Back</button>
             </Link>
